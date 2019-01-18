f08eb51a42dcf2924394704c010ac0123580b515
wooyoung <gunyoungc@gmail.com>
2019-01-18 21:33:54 +0900

    add data preprocess and reformat module add file handler for txt file and csv file

--- file_handler.py
+++ file_handler.py
@@ -1,15 +1,32 @@
 # coding: utf-8
 import csv, os, shutil
 import numpy as np
-from PIL import Image
 
-def loadData():
-    lines = loadCSV(getStoragePath()+'/resources/IAPS.csv')
-    lines = np.array(lines)
-    # images, images_data = loadImages(lines[:,0])
-    labels = np.argmax(lines[:,2:], axis=1)
-    # return images, images_data, labels
-    return lines[:,0], labels
+#########################################################################################################
+######################################### TXT ###########################################################
+
+def saveTxT(data, file_name, column_sec=[]):
+    if ".txt" not in file_name: file_name = file_name+".txt"
+
+    if ":\\" in file_name or ":/" in file_name : path = file_name.replace('\\','/')
+    else : path = getStoragePath()+"/data/data/"+file_name.replace('\\','/').replace(":", "")
+    directory = path[:path.rfind('/')]
+    if not os.path.isdir(directory):
+        makeDirectories(directory)
+
+    np.savetxt(fname=path, X=data, delimiter=' ')
+    return True
+
+def loadTxT(file_name, column_rows=0):
+    if ".txt" not in file_name: file_name = file_name+".txt"
+
+    if ":\\" in file_name or ":/" in file_name : path = file_name.replace('\\','/')
+    else : path = getStoragePath()+"/data/data/"+file_name.replace('\\','/').replace(":", "")
+    if not os.path.isfile(path) :
+        return None
+
+    lines = np.loadtxt(fname=path, delimiter=' ')
+    return lines[column_rows:]
 
 #########################################################################################################
 ######################################### CSV ###########################################################
@@ -21,7 +38,7 @@ def saveCSV(data, file_name, column_sec=[]):
     else : path = getStoragePath()+"/resources/"+file_name.replace('\\','/').replace(":", "")
     directory = path[:path.rfind('/')]
     if not os.path.isdir(directory):
-        os.makedirs(directory)
+        makeDirectories(directory)
 
     csv_file = open(path, "w", newline='\n')
     cw = csv.writer(csv_file, delimiter=',', quotechar='|')
@@ -53,44 +70,15 @@ def loadCSV(file_name, column_rows=0):
     return lines[column_rows:]
 
 #########################################################################################################
-######################################### IMAGE #########################################################
-def loadImages(file_list, shape=(28, 28, 1)):
-    images_data = np.array([])
-    width, height, kernel = shape
-    if len(file_list)>0:
-        for file_name in file_list:
-            img = loadJPG(file_name, height=height, width=width)
-            if shape[2] == 1:
-                img = img.convert('1')
-            img_w, img_h = img.size
-            image_data = np.array(img.getdata(), dtype=np.int_)
-            if img_w == width and img_h == height: image_data = image_data
-            else:
-                if img_w > width:
-                    image_data = image_data.reshape(img_h, img_w, kernel)[:,:width].reshape(img_h*width*kernel)
-                    img_w = width
-                elif img_w < width:
-                    image_data = np.concatenate((image_data.reshape(img_h, img_w, kernel), np.ones((img_h, width-img_w, kernel))*255), axis=1).reshape(img_h*width*kernel)
-                    img_w = width
-                if img_h > height:
-                    image_data = image_data.reshape(img_h, img_w, kernel)[:height].reshape(height * img_w * kernel)
-                elif img_h < height:
-                    image_data = np.concatenate((image_data.reshape(img_h, img_w, kernel), np.ones((height-img_h, img_w, kernel))*255), axis=0).reshape(height*width*kernel)
-
-            images_data = np.concatenate((images_data, image_data), axis=0)
-        return images_data.reshape(len(file_list), height, width, kernel), images_data.reshape(len(file_list), height*width*kernel)
-    else: None
-def loadJPG(file_name, height=28, width=28):
-    if ".jpg" not in file_name.lower(): file_name = file_name+".jpg"
-
-    if ":\\" in file_name or ":/" in file_name : path = file_name.replace('\\','/')
-    else : path = getStoragePath()+"/resources/resized_images/%dx%d/"%(height, width)+file_name.replace('\\','/').replace(":", "")
-    if not os.path.isfile(path) :
-        return None
-
-    return Image.open(path)
-
 #########################################################################################################
+def makeDirectories(directory):
+    if '\\' in directory: directory = directory.replace('\\', '/')
+    if '/' in directory:
+        u_dir = directory[:directory.rfind('/')]
+        if not os.path.isdir(u_dir):
+            makeDirectories(u_dir)
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
 def getStoragePath():
     StoragePath = os.getcwd().replace('\\', '/')
     return StoragePath
