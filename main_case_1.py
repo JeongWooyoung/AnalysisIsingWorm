12ed41ca62895db6ac7c23b16fbc990df90c0dfe
wooyoung <gunyoungc@gmail.com>
2019-01-21 00:43:47 +0900

    not using transfer handler order is s2 head to s4 tail(eglab_main) add case1 s2 head to s2 tail and s4 head to s4 tail

--- main_case_1.py
+++ main_case_1.py
@@ -1,33 +1,12 @@
 # coding: utf-8
-'''
-This script shows how to predict stock prices using a basic RNN
-'''
-
-# from __future__ import print_function
-#
-# import tensorflow as tf
-# import sys
-#
-# from eglab_model import LSTM
-#
-# def main(_):
-#     for filenum in range(1,50):
-#         train_model = LSTM(filenum)
-#         #train_model.data_train()
-#         train_model.data_test()
-#         tf.get_variable_scope().reuse_variables()
-#
-#     sys.exit(0)
-#
-# if __name__ == '__main__':
-#     tf.app.run()
-
 import numpy as np
 
 import arguments
 import file_handler as fh
 import evaluation_handler as eh
 
+# susceptibility head to tail
+# susceptibility4 head to tail
 trainFile = 'head'
 targetFile = 'tail'
 if __name__ == '__main__':
@@ -39,10 +18,18 @@ if __name__ == '__main__':
 
     args = arguments.parse_args()
     s2_train_data = train_data[:,:,1:2]
-    s2_target_data = target_data[:,:,2:3]
-    print("susceptibility head to susceptibility4 tail")
+    s2_target_data = target_data[:,:,1:2]
+    print("susceptibility")
     s2_result = eh.evaluations(args, s2_train_data, s2_target_data)
     s2_result = np.array(s2_result)
     print('=====================================================================================================================================================')
     print('S2 Average: loss %03.9f rmse: %03.5f' % (i + 1, np.mean(s2_result[:,1]), np.mean(s2_result[:,0])))
+    print('=====================================================================================================================================================')
+
+    s4_train_data = train_data[:,:,2:3]
+    s4_target_data = target_data[:,:,2:3]
+    print("susceptibility4")
+    eh.evaluations(args, s4_train_data, s4_target_data)
+    print('=====================================================================================================================================================')
+    print('S4 Average: loss %03.9f rmse: %03.5f' % (i + 1, np.mean(s2_result[:,1]), np.mean(s2_result[:,0])))
     print('=====================================================================================================================================================')
